fae9fdb5587fbac5831a0b91ba1096a1f866e2f8
MichelleTran <micheltran@deloitte.nl>
2021-11-25 14:01:24 +0100

    put prompts outside class

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
+UserID = input("Please type in your UserID: ")
+UserName = input("Please type in your name and press Enter: ")
+CashBalance = input("Your current Cash Balance is: ")
+Portfolio = input("Please enter your stock portfolio: ")
 
 class Users:
     # This class represents a user in the stock market
 
     def __init__(self, UserID, UserName, CashBalance, Portfolio):
-        self.UserID = input("Please type in your UserID: ")
-        self.UserName = input("Please type in your name and press Enter: ")
-        self.CashBalance = input("Your current Cash Balance is: ")
+        self.UserID = UserID
+        self.UserName = UserName
+        self.CashBalance = CashBalance
         self.Portfolio = []
 
-
-user1 = Users()
+user1 = Users(UserID, UserName, CashBalance, Portfolio)
+#user1 = Users(1, 'Michelle', 5000, ['stock1', 'stock2'])
